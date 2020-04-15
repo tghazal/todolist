6568bb1ffb9e497aa92d588007b886075cb9331f
unknown <t.bakeer4@gmail.com>
2020-04-14 19:05:38 -0700

    Updates

---
+++ precache-manifest.81d8b436b85a0ec4d5c01045b531de43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d6d52c19e38fd70cb7ff676d19e170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b03007ee195074c278b",
+    "url": "/static/css/main.75770b26.chunk.css"
+  },
+  {
+    "revision": "256ead1581b870ab3a39",
+    "url": "/static/js/2.4045bf55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4045bf55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b03007ee195074c278b",
+    "url": "/static/js/main.62a6ec7e.chunk.js"
+  },
+  {
+    "revision": "3c4b0cc570305db815fb",
+    "url": "/static/js/runtime-main.fee67b75.js"
+  }
+]);
